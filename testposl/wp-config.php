d69e1027f4fd973564475232c00831e297c6d159
Nasti <university18@yandex.ru>
2018-01-22 10:07:17 +0700

    Change file

--- testposl/wp-config.php
+++ testposl/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'testovaya');
+define('DB_NAME', 'web_64');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'web_64');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '6RN10unVxYNyUkew');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
